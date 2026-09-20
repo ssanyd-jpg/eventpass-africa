b6dc5da8c5ab61816f2be03cd11d0c884be95b53
ssanyd-jpg <ssanyd@gmail.com>
2026-09-20 12:20:46 +0300

    Match CHAAP hero stats to reference body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -109,10 +109,6 @@ export default async function HomePage() {
 
         <div className="chaap-reference-stats">
           <div>
-            <strong>{stats.totalEventsHosted.toLocaleString()}+</strong>
-            <span>EVENTS</span>
-          </div>
-          <div>
             <strong>{stats.totalTicketsSold.toLocaleString()}+</strong>
             <span>ATTENDEES</span>
           </div>
